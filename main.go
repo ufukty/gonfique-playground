db5f4526d174213224f6b03f9444864f993a72fc
Ufuk <13807261+ufukty@users.noreply.github.com>
2025-01-01 20:40:20 +0300

    main.go: improves logging

--- main.go
+++ main.go
@@ -6,8 +6,23 @@ import (
 	"net/http"
 )
 
+type LogerResponseWriter struct {
+	http.ResponseWriter
+	StatusCode int
+}
+
+func (l *LogerResponseWriter) WriteHeader(statusCode int) {
+	l.StatusCode = statusCode
+	l.ResponseWriter.WriteHeader(statusCode)
+}
+
 func Main() error {
-	err := http.ListenAndServe(":8080", (http.FileServer(http.Dir("static"))))
+	fs := http.FileServer(http.Dir("build"))
+	err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		l := &LogerResponseWriter{ResponseWriter: w}
+		fs.ServeHTTP(l, r)
+		fmt.Printf("%d %s %s\n", l.StatusCode, r.Method, r.RequestURI)
+	}))
 	if err != nil {
 		return fmt.Errorf("http.ListenAndServe: %w", err)
 	}
