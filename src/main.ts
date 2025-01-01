8f5121edcde2e231cc2a03efde8e85d245d53599
Ufuk <13807261+ufukty@users.noreply.github.com>
2025-01-01 14:39:10 +0300

    src/main: improves the example files

--- src/main.ts
+++ src/main.ts
@@ -29,51 +29,68 @@ declare function Convert(input: string, inputMode: string, config: string): [str
 let examples = {
     input: `domain: localhost
 gateways:
-public:
-path: /api/v1.0.0
-services:
-  document:
-    path: document
-    endpoints:
-      get: { method: "GET", path: "/" }
-      create: { method: "POST", path: "/" }
-      delete: { method: "DELETE", path: "/" }
-      patch: { method: "PATCH", path: "/" }
-  objectives:
-    path: tasks
-    endpoints:
-      get: { method: "GET", path: "/" }
-      create: { method: "POST", path: "/" }
-      delete: { method: "DELETE", path: "/" }
-      patch: { method: "PATCH", path: "/" }
-      put: { method: "PUT", path: "/" }
-  tags:
-    path: tags
-    endpoints:
-      get: { method: "GET", path: "/" }
-      create: { method: "POST", path: "/" }
-      delete: { method: "DELETE", path: "/" }
+  public:
+    path: /api/v1.0.0
+    services:
+      document:
+        path: document
+        endpoints:
+          get: { method: "GET", path: "/" }
+          create: { method: "POST", path: "/" }
+          delete: { method: "DELETE", path: "/" }
+          patch: { method: "PATCH", path: "/" }
+      objectives:
+        path: tasks
+        endpoints:
+          get: { method: "GET", path: "/" }
+          create: { method: "POST", path: "/" }
+          delete: { method: "DELETE", path: "/" }
+          patch: { method: "PATCH", path: "/" }
+          put: { method: "PUT", path: "/" }
+      tags:
+        path: tags
+        endpoints:
+          get: { method: "GET", path: "/" }
+          create: { method: "POST", path: "/" }
+          delete: { method: "DELETE", path: "/" }
 `,
-    config: `meta:
+    config: `### Please note that this version of Gonfique is a pre-alpha
+### version and it is still under development. 
+### Its design and implementation will change before release.
+
+### Gonfique Live (where you are now) is a separate project 
+### that puts Gonfique into your browser to let you try it 
+### before installing to system, and it is under development 
+### too.
+
+### Use links to provide feedback on each project and ask 
+### questions:
+### https://github.com/ufukty/gonfique
+### https://github.com/ufukty/gonfique-live
+
+### Toggle each comment block under rules section at once
+### to switch between demos
+
+meta:
   type: Config
 
 rules:
+
   "**": { export: true }
 
-  "**.objectives.endpoints": { declare: ObjectivesEndpoints }
-  "<ObjectivesEndpoints>": { iterator: true }
-  "<ObjectivesEndpoints>.*": { declare: Endpoint }
+  # "**.endpoints": { export: true }
+  # "**.endpoints.*": { declare: Endpoint }
 
-  "**.tags.endpoints": { declare: TagsEndpoints }
-  "<TagsEndpoints>": { iterator: true }
-  "<TagsEndpoints>.*": { declare: Endpoint }
+  # "**.path": { declare: Path }
+  # "**.endpoints.*": { declare: Endpoint }
+  # "<Endpoint>.path": { replace: Path }
+  # "<Endpoint>.method": { replace: "http.Method module/http" }
 
-  "**.document.endpoints": { declare: DocumentEndpoints }
-  "<DocumentEndpoints>": { iterator: true }
-  "<DocumentEndpoints>.*": { declare: Endpoint }
-
-  "<Endpoint>": { accessors: ["method", "path"] }
-  "<Endpoint>.method": { replace: http.Method test/http }
+  # "**.path": { declare: Path }
+  # "**.endpoints": { dict: map, declare: Endpoints }
+  # "**.endpoints.[value]": { declare: Endpoint }
+  # "<Endpoint>.path": { declare: Path }
+  # "<Endpoint>.method": { replace: "http.Method module/http" }
 `,
 };
 
